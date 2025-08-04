89634cddae64be451bb7ccf8f0ac7c95f63c3a9b
jibran <jibrankhot5@gmail.com>
2025-08-04 16:20:23 +0530

    project folder confirmed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,50 @@ const routes: Routes = [
         path: '',
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomeModule)
+      },
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./products/products.module').then((m) => m.ProductsModule)
+      },
+      {
+        path: 'categories',
+        loadChildren: () =>
+          import('./categories/categories.module').then((m) => m.CategoriesModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('./about/about.module').then((m) => m.AboutModule)
+      },
+      {
+        path: 'faq',
+        loadChildren: () =>
+          import('./faq/faq.module').then((m) => m.FaqModule)
+      },
+      {
+        path: 'blog',
+        loadChildren: () =>
+          import('./blog/blog.module').then((m) => m.BlogModule)
+      },
+      {
+        path: 'contact',
+        loadChildren: () =>
+          import('./contact/contact.module').then((m) => m.ContactModule)
+      },
+      {
+        path: 'product/:id',
+        loadChildren: () =>
+          import('./product-detail/product-detail.module').then((m) => m.ProductDetailModule)
       }
     ]
-  }
+  },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule) },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  { path: 'wishlist', loadChildren: () => import('./wishlist/wishlist.module').then(m => m.WishlistModule) }
 ];
 
 @NgModule({
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,24 +3,30 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
     TopBarComponent,
     NavbarComponent,
-    MainLayoutComponent],
+    HeaderComponent,
+    FooterComponent,
+    MainLayoutComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
     FontAwesomeModule
   ],
   exports: [
+    TopBarComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
     MainLayoutComponent
   ]
 })
